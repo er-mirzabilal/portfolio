04a14f9ec5700cec8f3996496052dd55efa81d7f
Sam-Hanjra <samhunjra83@gmail.com>
2022-12-11 21:18:42 +0500

    1st round completed

---
+++ components/animationtext.jsx
@@ -0,0 +1,24 @@
+
+
+const Animationtext = () => {
+  return (
+    <div className="h-screen ">
+      
+      <header className="mb-16 group">
+
+    <span
+      className=" inline-flex h-20 pt-2 overflow-x-hidden animate-type animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform"
+    >
+    Full Stack Web Developer 
+    </span>
+    <span
+      className="box-border inline-block w-1 ml-2 -mb-2 bg-white md:-mb-4 md:h-16 animate-cursor will-change-transform"
+    >    </span>
+
+
+</header>
+    </div>
+  )
+}
+
+export default Animationtext
---
+++ pages/data.jsx
@@ -0,0 +1,11 @@
+
+export const data = [
+ 
+    {
+        name: "Usama Saleem",
+        info: "FRONTEND Web developer",
+        img:"/mainpicture1.png"
+      },
+  ];
+ 
+  
